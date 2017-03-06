184143ce66d546e63e17e1a825233dd41a782aec
davidtnfsh <davidtnfsh@gmail.com>
2017-03-06 15:33:23 +0800

    turn GET & POST into View class, not separate view function

--- slothTw/urls.py
+++ slothTw/urls.py
@@ -5,6 +5,6 @@ from slothTw import views
 urlpatterns = [
   url(r'^get/clist$', views.clist, name='clist'),
   url(r'^get/cvalue$', views.cvalue, name='cvalue'),
-  url(r'^get/cComment$', views.cComment, name='cComment'),
-  url(r'^put/reply$', views.reply, name='reply'),
+  url(r'^get/comment$', views.Comment.as_view(), name='Comment'),
+  url(r'^put/reply$', views.Comment.as_view(), name='reply'),
 ]
